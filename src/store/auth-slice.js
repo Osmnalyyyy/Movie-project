48ebc1c8a9a18a983e453914706b75f8af06429d
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2023-01-22 11:36:11 +0300

    v5

---
+++ src/store/auth-slice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: [],
+  reducers: {
+    saveUser: (state, action) => {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { saveUser } = authSlice.actions;
+export default authSlice.reducer;
