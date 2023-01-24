907d9ff0fc201d700babdfeaec1d750d3dc5681d
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2023-01-24 16:24:03 +0300

    bootstrap-styled add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Cards from "../components/Cards";
 import Form from "../components/Form";
 import FormFilm from "../components/FormFilm";
 import { saveUser } from "../store/auth-slice";
+import { addMovie } from "../store/movie-data-slice";
 
 const user1 = {
   name: "osman",
@@ -14,28 +15,15 @@ const user1 = {
 
 const Home = () => {
   const dispatch = useDispatch();
-  const data = useSelector((state) => state);
+  const { auth, movie, data } = useSelector((state) => state);
 
-  const getMovie = async () => {
-    try {
-      const resp = await fetch(
-        "http://www.omdbapi.com/?t=cehennem&apikey=ce62ee3b"
-      )
-        .then((resp) => resp.json())
-        .then((data) => console.log(data));
-    } catch (error) {}
-  };
-
-  /*  useEffect(() => {
-    getMovie();
-  }, []); */
-
+  console.log(data);
   return (
     <div>
       <Cards />
       <FormFilm />
+      <button onClick={() => dispatch(addMovie(movie.movie))}>Add Movie</button>
       <button onClick={() => dispatch(saveUser(user1))}>Veri Gönder</button>
-      <button onClick={() => getMovie()}>Filmi Getir</button>
     </div>
   );
 };
