c48d2afa328342dcb8dfa02b0fdf258e37abf626
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2023-01-25 09:23:46 +0300

    film card add

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 
-const FilmCard = () => {
+const FilmCard = ({ title, Poster, imdbRating, Plot, year, Genre }) => {
   return (
-    <Card className="h-100 col-4 ml-4">
+    <Card className="h-100 col-3  m-3">
       <Card.Img
         variant="top"
         src="holder.js/100px180"
         style={{ width: "100%", height: "50%" }}
       />
       <Card.Body style={{ height: "50%" }}>
-        <Card.Title style={{ height: "10%" }}>Card Title</Card.Title>
-        <Card.Text style={{ height: "30%" }}>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title style={{ height: "10%" }}>{title}</Card.Title>
+        <Card.Text style={{ height: "30%" }}>{Plot}</Card.Text>
         <Button className="me-3" variant="success">
           Update Movie
         </Button>
