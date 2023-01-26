eb1b1a1e6e859f43c35bbb450135de2e922aa9b6
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2023-01-26 08:53:05 +0300

    modal add

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,18 +1,23 @@
-import React from "react";
-import { Container, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Modal, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import FilmCard from "./FilmCard";
+import ModalFilm from "./ModalFilm";
 
 const Cards = () => {
   const { data } = useSelector((state) => state);
-  console.log(data);
+  const [modalShow, setModalShow] = useState(false);
+  const [modalData, setModalData] = useState({ Title: "", imdbRating: "" });
+
   return (
     <Container>
       <Row>
         {data.map((item) => (
-          <FilmCard item={item} />
+          <FilmCard item={item} setModalShow={setModalShow} />
         ))}
       </Row>
+
+      <ModalFilm show={modalShow} onHide={() => setModalShow(false)} />
     </Container>
   );
 };
