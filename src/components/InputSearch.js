f3aa513a6078f353937cb05c3f860f75846434e5
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2023-01-30 12:48:56 +0300

    .

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 
-const InputSearch = ({ filteredData, setFilteredData }) => {
-  console.log(filteredData);
+const InputSearch = ({ filterData, setFilterData }) => {
   const handleNumber = (e) => {
     if (e.target.value > 10) {
-      setFilteredData({ ...filteredData, imdbRating: "10" });
+      setFilterData({ ...filterData, imdbRating: "10" });
     } else if (e.target.value < 0) {
-      setFilteredData({ ...filteredData, imdbRating: "0" });
+      setFilterData({ ...filterData, imdbRating: "0" });
     } else {
-      setFilteredData({ ...filteredData, imdbRating: e.target.value });
+      setFilterData({ ...filterData, imdbRating: e.target.value });
     }
   };
   return (
@@ -17,9 +16,9 @@ const InputSearch = ({ filteredData, setFilteredData }) => {
       <Form.Group className="mb-3">
         <Form.Label>Search Movie DB</Form.Label>
         <Form.Control
-          value={filteredData.Title}
+          value={filterData.Title}
           onChange={(e) =>
-            setFilteredData({ ...filteredData, Title: e.target.value })
+            setFilterData({ ...filterData, Title: e.target.value })
           }
           type="text"
           placeholder="input movie"
@@ -31,7 +30,7 @@ const InputSearch = ({ filteredData, setFilteredData }) => {
           onKeyDown={(evt) =>
             ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()
           }
-          value={filteredData.imdbRating}
+          value={filterData.imdbRating}
           onChange={(e) => handleNumber(e)}
           type="number"
           min={0}
--- src/components/SelectSearch.js
+++ src/components/SelectSearch.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 
-const SelectSearch = ({
-  UniqGenre,
-  UniqYears,
-  setFilteredData,
-  filteredData,
-}) => {
+const SelectSearch = ({ UniqGenre, UniqYears, setFilterData, filterData }) => {
   return (
     <>
-      <Form.Select aria-label="Default select example">
+      <Form.Select
+        onChange={(e) =>
+          setFilterData({ ...filterData, Genre: e.target.value })
+        }
+      >
         <option>Select Movie Genre</option>
         {UniqGenre?.map((item, i) => (
           <option value={item} key={i}>
@@ -17,7 +16,9 @@ const SelectSearch = ({
           </option>
         ))}
       </Form.Select>
-      <Form.Select>
+      <Form.Select
+        onChange={(e) => setFilterData({ ...filterData, Year: e.target.value })}
+      >
         <option>Select Movie Year</option>
         {UniqYears?.map((item, i) => (
           <option value={item} key={i}>
