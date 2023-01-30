abbafb719b66bdae8e4306cf6e86a28ced11ce92
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2023-01-30 09:44:56 +0300

    select search add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Cards from "../components/Cards";
 import Form from "../components/Form";
 import FormFilm from "../components/FormFilm";
+import InputSearch from "../components/InputSearch";
+import SelectSearch from "../components/SelectSearch";
 import { saveUser } from "../store/auth-slice";
 import { addMovie } from "../store/movie-data-slice";
 
@@ -11,6 +13,11 @@ const Home = () => {
   return (
     <div>
       <FormFilm />
+      <div>
+        <InputSearch />
+        <SelectSearch />
+        <SelectSearch />
+      </div>
       <Cards />
     </div>
   );
